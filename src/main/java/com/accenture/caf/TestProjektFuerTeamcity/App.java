682a6321324b742da321ffd8440847665c58e8cb
Nigg, Simon <simon.nigg@accenture.com>
2018-01-10 11:03:43 +0100

    Änderungen an Quellcode

--- src/main/java/com/accenture/caf/TestProjektFuerTeamcity/App.java
+++ src/main/java/com/accenture/caf/TestProjektFuerTeamcity/App.java
@@ -1,15 +1,10 @@
 package com.accenture.caf.TestProjektFuerTeamcity;
 
-/**
- * Hello world!
- *
- */
 public class App {
-	public static void main(String[] args) {
-		System.out.println("Hallo Kevin!");
-	}
+
+	private final static String name = "Kevin";
 
 	public static String hallo() {
-		return "Hallo Kevin";
+		return String.format("Hallo %s", name);
 	}
 }
