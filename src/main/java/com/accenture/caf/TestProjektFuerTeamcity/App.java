9c6755aff780b8fd79be42b373f13ccc4812c6b3
Nigg, Simon <simon.nigg@accenture.com>
2018-01-10 11:07:13 +0100

    Test soll fehlschlagen wegen Namensänderung

--- src/main/java/com/accenture/caf/TestProjektFuerTeamcity/App.java
+++ src/main/java/com/accenture/caf/TestProjektFuerTeamcity/App.java
@@ -2,7 +2,7 @@ package com.accenture.caf.TestProjektFuerTeamcity;
 
 public class App {
 
-	private final static String name = "Kevin";
+	private final static String name = "Simon";
 
 	public static String hallo() {
 		return String.format("Hallo %s", name);
