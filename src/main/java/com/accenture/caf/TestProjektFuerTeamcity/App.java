be316682fb0f9d72b03e6cfa101309fd2708cce3
Nigg, Simon <simon.nigg@accenture.com>
2018-01-10 10:12:16 +0100

    initialer Upload

---
+++ src/main/java/com/accenture/caf/TestProjektFuerTeamcity/App.java
@@ -0,0 +1,15 @@
+package com.accenture.caf.TestProjektFuerTeamcity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hallo Kevin!");
+	}
+
+	public static String hallo() {
+		return "Hallo Kevin";
+	}
+}
